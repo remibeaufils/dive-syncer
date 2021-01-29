473970a96fbf70e5eab1a7e5375e1093e30cc248
Rémi Beaufils <remi.beaufils@rappi.com>
2021-01-29 01:19:41 +0100

    Update calculation rules

--- src/shopify/lib/order-reducer.js
+++ src/shopify/lib/order-reducer.js
@@ -14,16 +14,19 @@ module.exports = ({ iana_timezone: shop_timezone }, order) => {
 
   const { refundLineItems, refundOrderAdjustments } = refunds.reduce(
     (acc, { refund_line_items, order_adjustments}) => {
-      refund_line_items.forEach(({ line_item_id, quantity, subtotal }) => {
-        if (!acc.refundLineItems[line_item_id])
-          acc.refundLineItems[line_item_id] = { quantity: 0, subtotal: 0 };
+      refund_line_items.forEach(({ line_item_id, quantity, subtotal, total_tax }) => {
+        if (!acc.refundLineItems[line_item_id]) {
+          acc.refundLineItems[line_item_id] = { quantity: 0, subtotal: 0, total_tax: 0 };
+        }
         acc.refundLineItems[line_item_id].quantity += quantity;
         acc.refundLineItems[line_item_id].subtotal += subtotal;
+        acc.refundLineItems[line_item_id].total_tax += total_tax;
       });
 
       order_adjustments.forEach(({ kind, amount }) => {
-        if (!acc.refundOrderAdjustments[kind])
+        if (!acc.refundOrderAdjustments[kind]) {
           acc.refundOrderAdjustments[kind] = 0;
+        }
         acc.refundOrderAdjustments[kind] += parseFloat(amount);
       });
 
@@ -32,16 +35,31 @@ module.exports = ({ iana_timezone: shop_timezone }, order) => {
     { refundLineItems: {}, refundOrderAdjustments: {}}
   );
 
+  // Ignored in API query.
+  // if (refundOrderAdjustments.refund_discrepancy) return [];
+
   // Calculate line items.
 
   let { order_item_quantity, lines } = line_items.reduce(
-    (acc, { id, variant_id, product_id, quantity, price, discount_allocations }) => {
-      const { quantity: refund_quantity, subtotal: refund_subtotal } = refundLineItems[id] || { quantity: 0, subtotal: 0 };
+    (
+      acc,
+      { id, variant_id, product_id, quantity, price, discount_allocations, tax_lines }
+    ) => {
+      const {
+        quantity: refund_quantity,
+        subtotal: refund_subtotal,
+        total_tax: refund_total_tax
+      } = refundLineItems[id] || { quantity: 0, subtotal: 0, total_tax: 0 };
 
       acc.order_item_quantity += quantity;
 
       const discountAllocations = discount_allocations.reduce(
         (acc, { amount }) => acc + parseFloat(amount),
+        0
+      );
+
+      const taxLines = tax_lines.reduce(
+        (acc, { price }) => acc + parseFloat(price),
         0
       );
 
@@ -56,9 +74,11 @@ module.exports = ({ iana_timezone: shop_timezone }, order) => {
         detail: {
           line_item_quantity: quantity,
           line_item_price: parseFloat(price),
+          line_item_tax_lines: taxLines,
           line_item_discount_allocations: discountAllocations,
-          refund_quantity,
-          refund_subtotal,
+          line_item_refund_quantity: refund_quantity,
+          line_item_refund_subtotal: refund_subtotal,
+          line_item_refund_total_tax: refund_total_tax,
         }
       });
 
@@ -74,7 +94,7 @@ module.exports = ({ iana_timezone: shop_timezone }, order) => {
     0
   );
 
-  const order_net_shipping = order_shipping_lines_price
+  const order_shipping_final = order_shipping_lines_price
     + (refundOrderAdjustments.shipping_refund || 0)
   ;
 
@@ -85,49 +105,60 @@ module.exports = ({ iana_timezone: shop_timezone }, order) => {
       detail: {
         line_item_quantity,
         line_item_price,
+        line_item_tax_lines,
         line_item_discount_allocations,
-        refund_quantity,
-        refund_subtotal,
+        line_item_refund_quantity,
+        line_item_refund_subtotal,
+        line_item_refund_total_tax,
       }
     } = line;
 
-    const line_item_shipping =
-      order_shipping_lines_price * line_item_quantity / order_item_quantity;
+    const line_item_ratio = line_item_quantity / order_item_quantity;
 
-    const line_item_turnover = line_item_price * line_item_quantity
+    const line_item_shipping = order_shipping_final * line_item_ratio;
+
+    const line_item_refund_discrepancy = Math.abs(
+      (refundOrderAdjustments.refund_discrepancy || 0) * line_item_ratio
+    );
+
+    const line_item_refund_total =
+      (line_item_refund_subtotal - line_item_refund_total_tax)
+      + line_item_refund_discrepancy;
+
+    let line_item_turnover =
+      (line_item_price - line_item_tax_lines) * line_item_quantity
       - line_item_discount_allocations
       + line_item_shipping
+      - line_item_refund_total
     ;
 
-    // const refund_discrepancy = line_item_quantity !== 0
-    //   ? (refundOrderAdjustments.refund_discrepancy || 0) * line_item_quantity / order_item_quantity
-    //   : 0
-    // ;
+    line_item_turnover = Math.round(line_item_turnover * 100) / 100;
 
-    const real_shipping_cost = line_item_shipping
+    // const real_shipping_cost = line_item_shipping_net
     // ^ Should be the real shipping cost because may vary from reality.
 
-    const line_item_profit = line_item_turnover
-      - refund_subtotal
-      - real_shipping_cost
+    // const line_item_profit = line_item_turnover
+    //   - refund_subtotal
+    //   - real_shipping_cost
       // - refund_discrepancy
       // - inventory cost
     ;
 
-    const profit_per_unit = line_item_profit / line_item_quantity;
+    // const profit_per_unit = line_item_profit / line_item_quantity;
 
     return {
       ...line,
       turnover: line_item_turnover,
       quantity: line_item_quantity,
-      profit: line_item_profit,
-      profit_per_unit,
+      // profit: line_item_profit,
+      // profit_per_unit,
       detail: {
         ...line.detail,
         line_item_shipping,
+        line_item_refund_discrepancy,
         order_item_quantity,
-        order_net_shipping,
         order_shipping_lines_price,
+        order_shipping_final,
         ...refundOrderAdjustments,
       }
     };
