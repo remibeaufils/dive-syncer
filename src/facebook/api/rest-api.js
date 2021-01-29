b3aa7884425ebac9c321c07ff7deda620d8ec05f
Rémi Beaufils <remi.beaufils@rappi.com>
2021-01-29 01:20:44 +0100

    Add get ad account endpoint

--- src/facebook/api/rest-api.js
+++ src/facebook/api/rest-api.js
@@ -1,15 +1,18 @@
 const axios = require('axios');
 
 const getEndpoint = (account_id) => {
-  return `https://graph.facebook.com/v9.0/${account_id}/insights`;
+  return `https://graph.facebook.com/v9.0/${account_id}`;
 };
 
-const getInsights = async (facebook, params) => {
+const getAccount = async (facebook, params) => {
   const { access_token, account_id } = facebook;
 
+  // https://developers.facebook.com/docs/marketing-api/reference/ad-account/
+  // { fields: 'currency,timezone_name' }
+  
   try {
     const response = await axios.get(
-      getEndpoint(account_id),
+      `${getEndpoint(account_id)}`,
       {
         params: {
           access_token,
@@ -26,4 +29,29 @@ const getInsights = async (facebook, params) => {
   }
 };
 
-module.exports = { getInsights };
+const getInsights = async (facebook, params) => {
+  const { access_token, account_id } = facebook;
+
+  // https://developers.facebook.com/docs/marketing-api/reference/ads-insights
+  // https://developers.facebook.com/docs/marketing-api/insights/parameters/v9.0
+
+  try {
+    const response = await axios.get(
+      `${getEndpoint(account_id)}/insights`,
+      {
+        params: {
+          access_token,
+          ...params
+        }
+      }
+    );
+
+    return response.data;    
+  } catch (error) {
+    console.log('\x1b[31mError: %s\x1b[0m', error.message);
+
+    return [];    
+  }
+};
+
+module.exports = { getAccount, getInsights };
